a52a15a9ded9b0ab1cdccfb9cab3b50374ecbef0
Štěpán Granát <granat.stepan@gmail.com>
2024-06-17 16:24:15 +0200

    fix: figma close on escape

--- src/ui/components/Dialog/Dialog.tsx
+++ src/ui/components/Dialog/Dialog.tsx
@@ -15,7 +15,12 @@ export const Dialog = ({ children, onClose }: Props) => {
   useWindowSize(DEFAULT_SIZE);
 
   useEffect(() => {
-    const handler = () => onClose();
+    const handler = (e: KeyboardEvent) => {
+      console.log(e);
+      if (e.code === "Escape") {
+        onClose();
+      }
+    };
     ref.current?.focus();
     ref.current?.addEventListener("keydown", handler);
     return () => ref.current?.removeEventListener("keydown", handler);
