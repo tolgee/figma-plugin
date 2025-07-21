74c3e111ac50fa5aceb5ebec92420d4f22fe4f92
Nico Hülscher <25116822+eweren@users.noreply.github.com>
2025-07-21 10:33:32 +0200

    adjust styling of namespaces to match new checkbox-dropdown-type-of-ui

--- src/ui/views/Settings/ProjectSettings.tsx
+++ src/ui/views/Settings/ProjectSettings.tsx
@@ -2,10 +2,17 @@ import { useApiQuery } from "@/ui/client/useQueryApi";
 import { FullPageLoading } from "@/ui/components/FullPageLoading/FullPageLoading";
 import { NamespaceSelect } from "@/ui/components/NamespaceSelect/NamespaceSelect";
 import { TolgeeConfig } from "@/types";
-import { VerticalSpace, Text, Muted, Checkbox } from "@create-figma-plugin/ui";
+import {
+  VerticalSpace,
+  Text,
+  Muted,
+  Checkbox,
+  Bold,
+} from "@create-figma-plugin/ui";
 import { Fragment, FunctionComponent, h } from "preact";
 import { useEffect, useState } from "preact/hooks";
 import styles from "./ProjectSettings.css";
+import { InfoTooltip } from "../../components/InfoTooltip/InfoTooltip";
 
 type Props = {
   apiKey: string;
@@ -13,6 +20,24 @@ type Props = {
   initialData?: Partial<TolgeeConfig>;
   onChange: (data: Partial<TolgeeConfig>) => void;
 };
+
+const namespaceHelpText = (
+  <Fragment>
+    <div>
+      For more information about namespaces,
+      <br />
+      check the{" "}
+      <a
+        href="https://docs.tolgee.io/js-sdk/namespaces"
+        target="_blank"
+        rel="noreferrer"
+      >
+        docs of the platform
+      </a>
+      .
+    </div>
+  </Fragment>
+);
 
 export const ProjectSettings: FunctionComponent<Props> = ({
   apiKey,
@@ -88,6 +113,16 @@ export const ProjectSettings: FunctionComponent<Props> = ({
     }
   }, [settings]);
 
+  const [showNamespaces, setShowNamespaces] = useState(
+    !initialData?.namespacesDisabled
+  );
+
+  const handleShowNamespaces = (e: any) => {
+    const checked = e.currentTarget.checked;
+    setShowNamespaces(checked);
+    setSettings({ ...settings!, namespacesDisabled: !checked });
+  };
+
   if (languagesLoadable.isLoading || namespacesLoadable.isLoading) {
     return <FullPageLoading />;
   }
@@ -116,34 +151,38 @@ export const ProjectSettings: FunctionComponent<Props> = ({
         ))}
       </select>
       <VerticalSpace space="medium" />
-      <Text>
-        <Muted>Default namespace</Muted>
-      </Text>
-      <VerticalSpace space="small" />
-      <div className={styles.namespacesRow}>
-        <NamespaceSelect
-          key={settings?.namespace || ""}
-          value={settings?.namespace || ""}
-          namespaces={namespaces}
-          onChange={(namespace) =>
-            setSettings((settings) => ({
-              ...settings!,
-              namespace,
-            }))
-          }
-        />
-        <Checkbox
-          value={Boolean(settings?.namespacesDisabled)}
-          onChange={(e) =>
-            setSettings((settings) => ({
-              ...settings!,
-              namespacesDisabled: Boolean(e.currentTarget.checked),
-            }))
-          }
-        >
-          <Text>Hide namespace selectors</Text>
+      <div className={styles.namespaceShowRow}>
+        <Checkbox value={showNamespaces} onChange={handleShowNamespaces}>
+          <Text>
+            <Bold>Show namespaces</Bold>
+          </Text>
         </Checkbox>
+
+        <InfoTooltip>{namespaceHelpText}</InfoTooltip>
       </div>
+      <VerticalSpace space="small" />
+      {showNamespaces && (
+        <Fragment>
+          <VerticalSpace space="extraSmall" />
+          <Text>
+            <Muted>Default namespace</Muted>
+          </Text>
+          <VerticalSpace space="small" />
+          <div className={styles.namespacesRow}>
+            <NamespaceSelect
+              key={settings?.namespace || ""}
+              value={settings?.namespace || ""}
+              namespaces={namespaces}
+              onChange={(namespace) =>
+                setSettings((settings) => ({
+                  ...settings!,
+                  namespace,
+                }))
+              }
+            />
+          </div>
+        </Fragment>
+      )}
     </Fragment>
   );
 };
