da2a8fc1356dc14bd4bcffa5f35947d8b6f51713
Štěpán Granát <granat.stepan@gmail.com>
2025-08-14 18:05:38 +0200

    chore: add retries in run mode

--- cypress.config.mjs
+++ cypress.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    retries: {
+      runMode: 2,
+      openMode: 0,
     },
   },
 });
--- cypress/common/tools.ts
+++ cypress/common/tools.ts
@@ -5,6 +5,4 @@ const ORIGIN = "http://localhost:3000";
 export const visitWithState = (data: Partial<PluginData>) => {
   cy.visit(`${ORIGIN}${createShortcutUrl(data)}`);
   cy.iframeReady();
-  cy.wait(100);
-  cy.iframeReady();
 };
