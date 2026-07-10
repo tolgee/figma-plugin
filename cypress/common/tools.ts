4ee86ebbaefe8f723e14a77978d5b3d2d1c35004
Nico Hülscher <25116822+eweren@users.noreply.github.com>
2026-07-10 18:32:22 +0200

    test(cypress): avoid stale iframe waits

--- cypress/common/tools.ts
+++ cypress/common/tools.ts
@@ -4,6 +4,14 @@ const ORIGIN = "http://localhost:3000";
 
 export const visitWithState = (data: Partial<PluginData>) => {
   cy.visit(`${ORIGIN}${createShortcutUrl(data)}`);
-  cy.wait(100);
-  cy.frameLoaded("#plugin_iframe");
+  cy.get<HTMLIFrameElement>("#plugin_iframe", { timeout: 30000 }).should(
+    ($iframe) => {
+      const body = $iframe[0]?.contentDocument?.body;
+      expect(body, "plugin iframe body").to.exist;
+      expect(
+        body!.childElementCount,
+        "plugin iframe content",
+      ).to.be.greaterThan(0);
+    },
+  );
 };
--- cypress/e2e/keyFormatEditor.cy.ts
+++ cypress/e2e/keyFormatEditor.cy.ts
@@ -16,27 +16,30 @@ describe("Key format editor", () => {
       },
     });
 
-    cy.iframe()
+    cy.iframeBody()
       .find('[data-cy="index_settings_button"]')
       .should("be.visible")
       .click();
-    cy.iframe()
+    cy.iframeBody()
       .find('[data-cy="settings_expandable_strings"]')
       .should("be.visible")
       .click();
-    cy.iframe().find('[data-cy="global-editor"]').should("be.visible").click();
-    cy.iframe().find(".cm-content").as("editorContent");
-    cy.iframe().find(".cm-tooltip-autocomplete").should("be.visible");
+    cy.iframeBody()
+      .find('[data-cy="global-editor"]')
+      .should("be.visible")
+      .click();
+    cy.iframeBody().find(".cm-content").as("editorContent");
+    cy.iframeBody().find(".cm-tooltip-autocomplete").should("be.visible");
     cy.wait(150);
 
     cy.get("@editorContent").type("{enter}").should("have.text", "artboard");
-    cy.iframe().find(".cm-tooltip-autocomplete").should("not.exist");
+    cy.iframeBody().find(".cm-tooltip-autocomplete").should("not.exist");
     cy.get("@editorContent").type(".component");
-    cy.iframe().find(".cm-tooltip-autocomplete").should("be.visible");
+    cy.iframeBody().find(".cm-tooltip-autocomplete").should("be.visible");
     cy.wait(150);
     cy.get("@editorContent")
       .type("{enter}")
       .should("have.text", "artboard.component");
-    cy.iframe().find(".cm-tooltip-autocomplete").should("not.exist");
+    cy.iframeBody().find(".cm-tooltip-autocomplete").should("not.exist");
   });
 });
