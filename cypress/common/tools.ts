1edc14f9f2926c2c0ee04ad208b7bed09202bde8
Nico Hülscher <25116822+eweren@users.noreply.github.com>
2026-07-10 18:58:42 +0200

    test(cypress): restore iframe setup

--- cypress/common/tools.ts
+++ cypress/common/tools.ts
@@ -4,14 +4,6 @@ const ORIGIN = "http://localhost:3000";
 
 export const visitWithState = (data: Partial<PluginData>) => {
   cy.visit(`${ORIGIN}${createShortcutUrl(data)}`);
-  cy.get<HTMLIFrameElement>("#plugin_iframe", { timeout: 30000 }).should(
-    ($iframe) => {
-      const body = $iframe[0]?.contentDocument?.body;
-      expect(body, "plugin iframe body").to.exist;
-      expect(
-        body!.childElementCount,
-        "plugin iframe content",
-      ).to.be.greaterThan(0);
-    },
-  );
+  cy.wait(100);
+  cy.frameLoaded("#plugin_iframe");
 };
